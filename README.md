1c4b5db3b26bffd4af1c95ef11a2f7a499f07ad7
Артем Попов <artempopov@MacBook-Pro-Artem-2.local>
2021-02-12 21:33:09 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# gyzno_bot
---
+++ main.py
@@ -0,0 +1,16 @@
+import telebot
+
+bot = telebot.TeleBot('1653349828:AAHvunYkNFShqra9gOpmLIInMSoXltesAsw')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши Привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True)
